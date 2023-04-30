32bd2a88575c3e09b33d08b279e3b1c9ec630a77
sahil6900 <sahilpawar33@gmail.com>
2023-04-30 15:20:31 +0530

    Aggregation functioning which can give oldest person and grouping by city

--- src/main/java/com/example/mongodbdemo/service/PersonServiceImpl.java
+++ src/main/java/com/example/mongodbdemo/service/PersonServiceImpl.java
@@ -2,10 +2,16 @@ package com.example.mongodbdemo.service;
 
 import com.example.mongodbdemo.collection.Person;
 import com.example.mongodbdemo.repository.PersonRepository;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
+import org.springframework.data.mongodb.core.aggregation.SortOperation;
+import org.springframework.data.mongodb.core.aggregation.UnwindOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.support.PageableExecutionUtils;
@@ -64,5 +70,17 @@ public class PersonServiceImpl implements PersonService{
                 pageable,()->mongoTemplate.count(query.skip(0).limit(0),Person.class));
 
         return people;
+    }
+
+    @Override
+    public List<Document> getOldestPersonByCity() {
+        UnwindOperation unwindOperation = Aggregation.unwind("addresses");
+        SortOperation sortOperation = Aggregation.sort(Sort.Direction.DESC,"age");
+        GroupOperation groupOperation = Aggregation.group("addresses.city").first(Aggregation.ROOT)
+                .as("oldestPerson");
+
+        Aggregation aggregation = Aggregation.newAggregation(unwindOperation,sortOperation,groupOperation);
+        List<Document> person = mongoTemplate.aggregate(aggregation,Person.class, Document.class).getMappedResults();
+        return person;
     }
 }
